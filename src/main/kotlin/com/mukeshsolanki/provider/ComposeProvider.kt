ada6c564f915e539173df56acb6c07c505766e20
Mukesh Solanki <me@mukeshsolanki.com>
2023-02-04 15:09:33 +0530

    Initial Commit

--- src/main/kotlin/com/mukeshsolanki/provider/ComposeProvider.kt
+++ src/main/kotlin/com/mukeshsolanki/provider/ComposeProvider.kt
@@ -1,16 +1,19 @@
-package com.mukeshsolanki.rules
+package com.mukeshsolanki.provider
 
+import com.mukeshsolanki.rules.DpInComposable
+import com.mukeshsolanki.rules.LargeClass
 import io.gitlab.arturbosch.detekt.api.Config
 import io.gitlab.arturbosch.detekt.api.RuleSet
 import io.gitlab.arturbosch.detekt.api.RuleSetProvider
 
-class CustomProvider : RuleSetProvider {
+class ComposeProvider : RuleSetProvider {
 
-    override val ruleSetId: String = "custom"
+    override val ruleSetId: String = "compose"
 
     override fun instance(config: Config): RuleSet = RuleSet(
         ruleSetId,
         listOf(
+            DpInComposable(config),
             LargeClass(config),
         )
     )
---
+++ src/main/kotlin/com/mukeshsolanki/rules/DpInComposable.kt
@@ -0,0 +1,26 @@
+package com.mukeshsolanki.rules
+
+import io.gitlab.arturbosch.detekt.api.*
+import org.jetbrains.kotlin.psi.KtDotQualifiedExpression
+
+class DpInComposable(config: Config) : Rule(config) {
+    override val issue = Issue(
+        id = "DpInComposable",
+        severity = Severity.Defect,
+        description = "Composables contains hardcoded dp values.",
+        Debt.FIVE_MINS
+    )
+
+    override fun visitDotQualifiedExpression(expression: KtDotQualifiedExpression) {
+        val selector = expression.selectorExpression ?: return
+        if (selector.text == "dp") {
+            report(
+                CodeSmell(
+                    issue,
+                    Entity.from(expression),
+                    "Use of `dp` in Compose functions is discouraged, consider creating a constant"
+                )
+            )
+        }
+    }
+}
